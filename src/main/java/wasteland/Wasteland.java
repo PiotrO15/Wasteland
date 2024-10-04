5848c071b98fe180eda794f04e9bd7ded5f0f35a
Piotr015 <piotr.bogacki0@gmail.com>
2024-10-04 21:32:02 +0200

    Remove pointless code from main class

--- src/main/java/wasteland/Wasteland.java
+++ src/main/java/wasteland/Wasteland.java
@@ -1,79 +1,31 @@
 package wasteland;
 
-import com.mojang.logging.LogUtils;
-import net.minecraft.client.Minecraft;
-import net.minecraft.world.level.block.Blocks;
-import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.minecraftforge.registries.ForgeRegistries;
-import org.slf4j.Logger;
 import wasteland.block.ModBlocks;
 import wasteland.item.Compost;
 import wasteland.item.EssenceItem;
 import wasteland.item.ModItems;
 import wasteland.item.ResonantRod;
 
-// The value here should match an entry in the META-INF/mods.toml file
 @Mod(Wasteland.MOD_ID)
 public class Wasteland {
-
-    // Define mod id in a common place for everything to reference
     public static final String MOD_ID = "wasteland";
-    // Directly reference a slf4j logger
-    private static final Logger LOGGER = LogUtils.getLogger();
-    // Create a Deferred Register to hold Blocks which will all be registered under the "Wasteland" namespace
-
 
     public Wasteland() {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
-        // Register the commonSetup method for modloading
-        modEventBus.addListener(this::commonSetup);
-
-        // Register the Deferred Register to the mod event bus so blocks get registered
         ModBlocks.register(modEventBus);
-        // Register the Deferred Register to the mod event bus so items get registered
         ModItems.register(modEventBus);
 
         modEventBus.addListener(ModBlocks::buildContents);
         modEventBus.addListener(ModItems::buildContents);
 
-        // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
         MinecraftForge.EVENT_BUS.register(EssenceItem.class);
         MinecraftForge.EVENT_BUS.register(Compost.class);
         MinecraftForge.EVENT_BUS.register(ResonantRod.class);
-    }
-
-    private void commonSetup(final FMLCommonSetupEvent event) {
-        // Some common setup code
-        LOGGER.info("HELLO FROM COMMON SETUP");
-        LOGGER.info("DIRT BLOCK >> {}", ForgeRegistries.BLOCKS.getKey(Blocks.DIRT));
-    }
-
-    // You can use SubscribeEvent and let the Event Bus discover methods to call
-    @SubscribeEvent
-    public void onServerStarting(ServerStartingEvent event) {
-        // Do something when the server starts
-        LOGGER.info("HELLO from server starting");
-    }
-
-    // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
-    @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
-    public static class ClientModEvents {
-
-        @SubscribeEvent
-        public static void onClientSetup(FMLClientSetupEvent event) {
-            // Some client setup code
-            LOGGER.info("HELLO FROM CLIENT SETUP");
-            LOGGER.info("MINECRAFT NAME >> {}", Minecraft.getInstance().getUser().getName());
-        }
     }
 }
