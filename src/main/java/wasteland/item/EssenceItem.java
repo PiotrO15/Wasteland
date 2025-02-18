195634fe1d6a1fe030507ff9c9da67aa26114b4f
PiotrO15 <72670117+PiotrO15@users.noreply.github.com>
2025-02-18 13:08:33 +0100

    Remove some of the testing code, fix chunks not being saved

--- src/main/java/wasteland/item/EssenceItem.java
+++ src/main/java/wasteland/item/EssenceItem.java
@@ -3,12 +3,9 @@ package wasteland.item;
 import net.minecraft.core.*;
 import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.core.registries.Registries;
-import net.minecraft.network.chat.ChatType;
-import net.minecraft.network.chat.OutgoingChatMessage;
-import net.minecraft.network.chat.PlayerChatMessage;
+import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerLevel;
-import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.biome.Biome;
 import net.minecraft.world.level.biome.BiomeResolver;
@@ -17,7 +14,6 @@ import net.minecraft.world.level.chunk.ChunkStatus;
 import net.minecraft.world.level.levelgen.structure.BoundingBox;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
-import org.apache.commons.lang3.mutable.MutableInt;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -40,8 +36,8 @@ public class EssenceItem {
             }
 
             // Set the biome at the location of use
-            BlockPos quantizedPos1 = quantize(new BlockPos(pos.getX() - 2, -64, pos.getZ() - 2));
-            BlockPos quantizedPos2 = quantize(new BlockPos(pos.getX() + 2, 320, pos.getZ() + 2));
+            BlockPos quantizedPos1 = quantize(new BlockPos(pos.getX() - 2, level.getMinBuildHeight(), pos.getZ() - 2));
+            BlockPos quantizedPos2 = quantize(new BlockPos(pos.getX() + 2, level.getMaxBuildHeight(), pos.getZ() + 2));
             BoundingBox boundingBox = BoundingBox.fromCorners(quantizedPos1, quantizedPos2);
 
             ServerLevel serverLevel = (ServerLevel) level;
@@ -49,33 +45,23 @@ public class EssenceItem {
 
             for(int k = SectionPos.blockToSectionCoord(boundingBox.minZ()); k <= SectionPos.blockToSectionCoord(boundingBox.maxZ()); ++k) {
                 for(int l = SectionPos.blockToSectionCoord(boundingBox.minX()); l <= SectionPos.blockToSectionCoord(boundingBox.maxX()); ++l) {
-                    ChunkAccess chunkaccess = serverLevel.getChunk(l, k, ChunkStatus.FULL, false);
-
-                    chunkAccessList.add(chunkaccess);
+                    ChunkAccess chunkAccess = serverLevel.getChunk(l, k, ChunkStatus.FULL, false);
+                    if (chunkAccess != null)
+                        chunkAccessList.add(chunkAccess);
                 }
             }
 
-            MutableInt mutableInt = new MutableInt(0);
-
             for(ChunkAccess chunkAccess : chunkAccessList) {
-                ResourceLocation resourceLocation = serverLevel.registryAccess().registryOrThrow(Registries.BIOME).getKey(chunkAccess.getNoiseBiome(0, 0, 0).get());
+                ResourceLocation resourceLocation = serverLevel.registryAccess().registryOrThrow(Registries.BIOME).getKey(chunkAccess.getNoiseBiome(0, chunkAccess.getHeight(), 0).get());
                 if (resourceLocation != null && resourceLocation.getNamespace().equals("wasteland")) {
-                    Biome biome = serverLevel.registryAccess().registryOrThrow(Registries.BIOME).get(new ResourceLocation("minecraft", resourceLocation.getPath()));
-                    if (biome != null) {
-                        Holder<Biome> biomeHolder = Holder.direct(biome);
-                        chunkAccess.fillBiomesFromNoise(makeResolver(mutableInt, chunkAccess, boundingBox, biomeHolder, (p_262543_) -> true), serverLevel.getChunkSource().randomState().sampler());
-                        chunkAccess.setUnsaved(true);
-                    }
+                    Holder<Biome> biomeHolder = serverLevel.registryAccess().registryOrThrow(Registries.BIOME).getHolderOrThrow(ResourceKey.create(Registries.BIOME, new ResourceLocation("minecraft", resourceLocation.getPath())));
+
+                    chunkAccess.fillBiomesFromNoise(makeResolver(chunkAccess, boundingBox, biomeHolder, (p_262543_) -> true), serverLevel.getChunkSource().randomState().sampler());
+                    chunkAccess.setUnsaved(true);
                 }
             }
 
             serverLevel.getChunkSource().chunkMap.resendBiomesForChunks(chunkAccessList);
-
-            Player player = event.getEntity();
-
-            PlayerChatMessage chatMessage = PlayerChatMessage.unsigned(player.getUUID(), "Set " + mutableInt + " blocks to plains from " + boundingBox.minX() + ", " + boundingBox.minY() + ", " + boundingBox.minZ() + " to " + boundingBox.maxX() + ", " + boundingBox.maxY() + ", " + boundingBox.maxZ());
-
-            player.createCommandSourceStack().sendChatMessage(new OutgoingChatMessage.Player(chatMessage), false, ChatType.bind(ChatType.CHAT, player));
         }
     }
 
@@ -87,14 +73,13 @@ public class EssenceItem {
         return new BlockPos(quantize(blockPos.getX()), quantize(blockPos.getY()), quantize(blockPos.getZ()));
     }
 
-    private static BiomeResolver makeResolver(MutableInt mutableInt, ChunkAccess chunkAccess, BoundingBox boundingBox, Holder<Biome> p_262705_, Predicate<Holder<Biome>> p_262695_) {
+    private static BiomeResolver makeResolver(ChunkAccess chunkAccess, BoundingBox boundingBox, Holder<Biome> p_262705_, Predicate<Holder<Biome>> p_262695_) {
         return (p_262550_, p_262551_, p_262552_, p_262553_) -> {
             int i = QuartPos.toBlock(p_262550_);
             int j = QuartPos.toBlock(p_262551_);
             int k = QuartPos.toBlock(p_262552_);
             Holder<Biome> holder = chunkAccess.getNoiseBiome(p_262550_, p_262551_, p_262552_);
             if (boundingBox.isInside(i, j, k) && p_262695_.test(holder)) {
-                mutableInt.increment();
                 return p_262705_;
             } else {
                 return holder;
